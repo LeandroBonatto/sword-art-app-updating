cfb9704ed154043f01b9638348a303ff47175683
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-04 12:13:17 -0400

    @chacra-ui/react

---
+++ src/components/CharacterSelection/CharacterSelection.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text, Checkbox, CheckboxGroup} from '@chacra-ui/react'
+
+export const CharacterSelection = () => {
+  return (
+    <div>CharacterSelection</div>
+  )
+}
