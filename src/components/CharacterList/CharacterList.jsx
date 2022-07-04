0f05ed9a5c52f09c9393d1a88d1489457851b97c
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-04 13:22:36 -0400

    CharacterSelec

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -5,7 +5,6 @@ import "./CharacterList.css"
 // Props are passed to the component via attributes
 export const CharacterList = (characters) => {
     //returns true if math.random is more than 0.5
-    const isChampion = Math.random() > 0.5;
   return (
     <ul>
         {characters.map((character) => (
--- src/components/CharacterSelection/CharacterSelection.tsx
+++ src/components/CharacterSelection/CharacterSelection.tsx
@@ -4,7 +4,7 @@ import { Text, Checkbox, CheckboxGroup, Stack } from "@chakra-ui/react";
 export const CharacterSelection = ({ characters: any }) => {
   return (
     <Stack spacing={5}>
-        <Text fontSize={"4xl"}>Select your champuons!</Text>
+        <Text fontSize={"4x1"}>Select your champuons!</Text>
         <CheckboxGroup colorScheme="green">
             <Stack spacing={[1, 5]} direction={["column", "row"]}>
                 {characters.map((character: any) => (
