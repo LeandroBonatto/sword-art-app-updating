c88507471210bdaedafdabb7f6287799b13d52be
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-04 15:35:42 -0400

    Tbody

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CharacterListItem } from './CharacterListItem/CharacterListItem';
-import { Table, TableCaption, Thead, Tr, Th } from "@chakra-ui/react"
+import { Table, TableCaption, Thead, Tr, Th, Tbody } from "@chakra-ui/react"
 import "./CharacterList.css";
 
 // Props are passed to the component via attributes
@@ -9,16 +9,23 @@ export const CharacterList = (characters) => {
   return (
     <Table>
         <TableCaption>Imperial to metric conversion Factors</TableCaption>
-    <Thead>
-      <tr>
-        <th>To convert</th>
-      </tr>
-    </Thead>
-
-
-        {characters.map((character) => (
-        <CharacterListItem isChampion={Math.random() > 0.5} character={character} />
-        ))}
+      <Thead>
+        <tr>
+          <th>Name</th>
+          <th isNumeric>Helath</th>
+          <th>Fraction</th>
+          <th>Weapon</th>
+          <th isNumeric>Damage per Hit</th>
+        </tr>
+        </Thead>
+        <Tbody>
+          {characters.map((character) => (
+            <CharacterListItem 
+              isChampion={Math.random() > 0.5} 
+              character={character}  
+            />
+          ))}
+        </Tbody>
     </Table>
   );
 };
