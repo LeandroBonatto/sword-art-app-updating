d71f2f65718b93b0d0827c6df3b1c2f9a8210876
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-12 13:37:21 -0400

    CharacterSelection

--- src/components/CharacterSelection/CharacterSelection.tsx
+++ src/components/CharacterSelection/CharacterSelection.tsx
@@ -2,19 +2,28 @@ import React from 'react'
 import { Text, Checkbox, CheckboxGroup, Flex, Stack } from "@chakra-ui/react";
 
 export const CharacterSelection = ({ characters }) => {
-    const [heroesSelected, setHeroesSelected] = useState([]);
-    const onHeroChanged = (event, hero) => {
+    const [heroesSelected, setHeroesSelected] = useState<array<string>>([]);
+    const onHeroChanged = (event, hero: string) => {
+        if (heroesSelected.includes(hero)) {
+            setHeroesSelected(heroesSelected.filter((h) => h !== hero));
+        } else {
+            setHeroesSelected([...heroesSelected, hero]);
+        }
     };
-
   return (
     <Flex justify={"center"}align={"center"} direction={"column"}>
         <Text fontSize={"4xl"}>Select your champions!</Text>
         <CheckboxGroup colorScheme="green">
             <Stack spacing={[1, 5]} direction={["column", "row"]}>
                 {characters.map((character: any) => (
-                    <Checkbox isChecked value={character.name} key={character.name}>
-                        {character.name}
-                    </Checkbox>
+                <Checkbox 
+                    isChecked = {heroesSelected.includes(character.name)}
+                    onChange={(e) => onHeroChanged(e, character.name)}
+                    value={character.name} 
+                    key={character.name}
+                    >
+                    {character.name}
+                </Checkbox>
                 ))}
             </Stack>
         </CheckboxGroup>
