99c0ec4008daea01ba29a7bdb8eeff65fe8b55de
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-13 11:23:28 -0400

    savedCallbackRef

--- src/hooks/useinterval.ts
+++ src/hooks/useinterval.ts
@@ -1,8 +1,15 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
+//This hook is used to run a function every x seconds
 export const useInterval = (
     callback: () => void,
     timeInterval: number | null
 ) => {
+    //Our function (callback) is accessible by savedCallbackRef.current
     const savedCallBack = useRef(callback);
+    useEffect(() => {
+        if (!timeInterval || timeInterval === 0) {
+            return;
+        }
+    }
 };
