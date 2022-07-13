eb05f1d869eb65c8fcf3f5e35ac0f36c2d42c7be
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-13 10:58:06 -0400

    firstFighterState

--- src/components/Battleground/Battleground.jsx
+++ src/components/Battleground/Battleground.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { Box, Text, Flex } from "@chakra-ui/react";
 
 export const Battleground = ({ battleCharacters }) => {
+    const [fighter1, fighter2] = battleCharacters;
+    const [firstFighterState, setFirstFighterState] = React.useState(fighter1);
+    const [secondFighterState, setSecondFighterState] = React.useState(fighter2);
     return (
         <Flex justify={"center"}align={"center"} direction={"column"} h="90vh">
             <text mt="2%" fontSize={"3xl"} fontWeight="700" >
             Let's get ready to the fight
             </text>
-            <Box w="80%" h="100%" mt="5%" border="0.5rem solid black">
-            <text mt="5%" fontSize={"2xl"} fontWeight="600" >
+            <Text mt="2%" fontSize={"2xl"} fontWeight="600">
+                {firstFighterState.name} health: {firstFighterState.health}
+            </Text>
+            <Text mt="2%" fontSize={"2xl"} fontWeight="600">
+                {secondFighterState.name} health: {secondFighterState.health}
+            </Text>
+            <Box w="80%" h="100%" mt="3%" border="0.5rem solid black">
+            <text mt="2%" fontSize={"2xl"} fontWeight="600" >
             Let's get ready to the fight
             </text>
-            <text mt="38%" fontSize={"2xl"} fontWeight="600" >
+            <text mt="35%" fontSize={"2xl"} fontWeight="500" >
             Let's get ready to the fight
             </text>
             </Box>
