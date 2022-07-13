9d5d096b7b2d386c262d9efb16beec09653b74ee
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-13 10:47:15 -0400

    battleCharac

--- src/components/Battleground/Battleground.jsx
+++ src/components/Battleground/Battleground.jsx
@@ -4,10 +4,16 @@ import { Box, Text, Flex } from "@chakra-ui/react";
 export const Battleground = ({ battleCharacters }) => {
     return (
         <Flex justify={"center"}align={"center"} direction={"column"} h="90vh">
-            <text mt="5%" fontSize={"3xl"} fontWeight="700" >
+            <text mt="2%" fontSize={"3xl"} fontWeight="700" >
             Let's get ready to the fight
             </text>
-            <Box w="80" h="100%" mt="5%" border="0.5rem solid black">
+            <Box w="80%" h="100%" mt="5%" border="0.5rem solid black">
+            <text mt="5%" fontSize={"2xl"} fontWeight="600" >
+            Let's get ready to the fight
+            </text>
+            <text mt="38%" fontSize={"2xl"} fontWeight="600" >
+            Let's get ready to the fight
+            </text>
             </Box>
         </Flex>
     );
