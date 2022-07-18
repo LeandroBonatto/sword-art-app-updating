af9b3104f28747a41fa14f3885fd09b6b4778d8a
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-17 23:33:19 -0400

    WinnerScreen

--- src/screens/WinnerScreen.tsx
+++ src/screens/WinnerScreen.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Text } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 
-export const WinnerScreen = () => {
+export const WinnerScreen = ({ winer, setFightStart }) => {
     return (
         <>
             <Text> fontSize={"5xl"} fontWeight"800">
                 Winner of the battle is {winner}
             </Text>
-            <Button>Go back to character screen</Button>
+            <Button mt="3%" onClick={() => setFightStart}>
+                Go back to character screen
+            </Button>
         </>
     )
 }
