9355a8b357414bf0e5743736a9f77d725a1be98f
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-22 19:47:02 -0400

    configureStore

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+//Redux store is a single source of truth for the application state
+//Note: we can still combine react useState and redux store
+export default configureStore({
+    reducer: {},
+})
