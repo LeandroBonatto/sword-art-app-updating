64bf5b2e97288c426d84f606773c6885d774313b
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-29 11:43:43 -0400

    setIsLoggedIn

--- src/components/slices/loginSlice.js
+++ src/components/slices/loginSlice.js
@@ -1,3 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-export const loginSlice = createSlice
+export const loginSlice = createSlice({
+    name: "login",
+    initialState: {
+        isLoggedIn: false,
+    },
+    reducers: {
+        setIsLoggedIn: (state, action) => {
+            return {
+                isLoggedIn: action.payload,
+            }
+        }
+    }
+});
