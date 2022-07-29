deec12340753796cd9c14f8e2a8b8d82f22eebf7
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-07-29 19:31:06 -0400

    cahnges

--- src/screens/CharactersScreen.tsx
+++ src/screens/CharactersScreen.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { CharacterList } from "../components/CharacterList/CharacterList";
 import { CharacterSelection } from "../components/CharacterSelection/CharacterSelection";
 
 //Screens are composed of components and they group what we want to see on the screen at one time
 export const CharactersScreen = ({ 
-    isLoggedIn,
 }) => {
     const navigate = useNavigate();
 
+    const isLoggedIn = useSelector((store: any) => store.login.isLoggedIn);
     if(!isLoggedIn) {
         navigate("/");
     }
