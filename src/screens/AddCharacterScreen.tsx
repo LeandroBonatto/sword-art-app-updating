84dc9eba37b32a2b1ee05c71e6a2677150445d5a
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-08-15 13:35:22 -0400

    UseState

--- src/screens/AddCharacterScreen.tsx
+++ src/screens/AddCharacterScreen.tsx
@@ -1,19 +1,24 @@
 import { InputGroup, Stack, InputLeftElement, Input, InputRightElement } from "@chakra-ui/react";
 import { PhoneIcon, CheckIcon} from "@chakra-ui/react";
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddCharacterScreen = () => {
+  const [name, setName] = useState("");
+    const [damage, setDamagePerHit] = useState(0);
+    const [health, setHealth] = useState(0);
+    const [fraction, setFraction] = useState("");
+    const [weapon, setWeapon] = useState("");
   return (
     <Stack spacing={4}>
       <Input type='text' placeholder="Please enter a character name" />
       <Input type='text' placeholder="Please enter a character fraction" />
       <Input type='number' placeholder="Please enter a character health" />
-
       <Input 
         type="number"
         placeholder="Please enter a character damage per hit" 
       />
       <Input type='number' placeholder="Please enter a character weapon" />
+      <button>Add character</button>
     </Stack>
   );
 };
