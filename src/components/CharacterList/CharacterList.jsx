d18adde443dcf488d440947b238f7f5b0ce63365
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-08-15 12:02:13 -0400

    Tips

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -6,6 +6,12 @@ import { useSelector } from "react-redux";
 
 
 // Props are passed to the component via attributes
+
+//To implement character creation and deletion we need to do these things:
+//1. We need to create Add/Update screen which will be responsible for taking input for create/update
+//2. We need to implement delete button on each row of the table
+//3. We need to implement appropriate actions in our characters slice of the state
+//4. We need to have a button to create new character 
 export const CharacterList = () => {
   //by using useSelector we basically get small piece of data from the srote
   //and we subscribe to it
