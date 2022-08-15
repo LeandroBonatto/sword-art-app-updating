bdbfd85d36dea80a5b5f8b48b0cd04facd6587d9
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-08-15 15:23:31 -0400

    validateValues

--- src/screens/AddCharacterScreen.tsx
+++ src/screens/AddCharacterScreen.tsx
@@ -1,25 +1,58 @@
-import { InputGroup, Stack, InputLeftElement, Input, InputRightElement } from "@chakra-ui/react";
+import { InputGroup, Stack, InputLeftElement, Input, InputRightElement, Alert, AlertIcon } from "@chakra-ui/react";
 import { PhoneIcon, CheckIcon} from "@chakra-ui/react";
 import React, { useState } from 'react'
 
 export const AddCharacterScreen = () => {
-  const [name, setName] = useState("");
-    const [damage, setDamagePerHit] = useState(0);
+    const [name, setName] = useState("");
+    const [damagePetHit, setDamagePerHit] = useState(0);
     const [health, setHealth] = useState(0);
     const [fraction, setFraction] = useState("");
     const [weapon, setWeapon] = useState("");
+
+    const validateValues =>
+
+    const handleCharacterAdd = () => {
+    }
+
+    const alert = (
+      <Alert status="error">
+          <AlertIcon />
+          Please make sure your imports are valid!
+      </Alert>
+  );
   return (
     <Stack spacing={4}>
-      <Input type='text' value={name} placeholder="Please enter a character name" />
-      <Input type='text' value={fraction} placeholder="Please enter a character fraction" />
-      <Input type='number' value={health} placeholder="Please enter a character health" />
+    <Input 
+      type='text' 
+      value={name} 
+      placeholder="Please enter a character name"
+      onChange={(e) => setName(e.target.value)}
+    />
+    <Input 
+      type='text' 
+      value={fraction} 
+      placeholder="Please enter a character fraction" 
+      onChange={(e) => setFraction(e.target.value)}
+    />
+      <Input 
+        type='number' 
+        value={health} 
+        placeholder="Please enter a character health" 
+        onChange={(e) => setHealth(Number(e.target.value))}
+      />
       <Input 
         type="number"
         value={damagePerHit}
         placeholder="Please enter a character damage per hit" 
+        onChange={(e) => setDamagePerHit(Number(e.target.value))}
       />
-      <Input type='text' value={weapon} placeholder="Please enter a character weapon" />
-      <button>Add character</button>
+      <Input 
+        type='text' 
+        value={weapon}
+        onChange={(e) => setWeapon(e.target.value)} 
+        placeholder="Please enter a character weapon" 
+      />
+      <Button>Add character</Button>
     </Stack>
   );
 };
