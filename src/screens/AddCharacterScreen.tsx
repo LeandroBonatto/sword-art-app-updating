cbcf2ef072f4308a2e017fc250de0ec0d9abd521
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2022-08-15 13:02:43 -0400

    AddCharacScreen

--- src/screens/AddCharacterScreen.tsx
+++ src/screens/AddCharacterScreen.tsx
@@ -5,42 +5,15 @@ import React from 'react'
 export const AddCharacterScreen = () => {
   return (
     <Stack spacing={4}>
-    <InputGroup>
-      <InputLeftElement
-        pointerEvents='none'
-        children={<PhoneIcon color='gray.300' />}
-      />
       <Input type='text' placeholder="Please enter a character name" />
-    </InputGroup>
-
-    <Stack spacing={4}>
-    <InputGroup>
-      <InputLeftElement
-        pointerEvents='none'
-        children={<PhoneIcon color='gray.300' />}
-      />
       <Input type='text' placeholder="Please enter a character fraction" />
-    </InputGroup>
-
-    <Stack spacing={4}>
-    <InputGroup>
-      <InputLeftElement
-        pointerEvents='none'
-        children={<PhoneIcon color='gray.300' />}
-      />
       <Input type='number' placeholder="Please enter a character health" />
-    </InputGroup>
-  
-    <InputGroup>
-      <InputLeftElement
-        pointerEvents='none'
-        color='gray.300'
-        fontSize='1.2em'
-        children='$'
+
+      <Input 
+        type="number"
+        placeholder="Please enter a character damage per hit" 
       />
-      <Input placeholder='Enter amount' />
-      <InputRightElement children={<CheckIcon color='green.500' />} />
-    </InputGroup>
-  </Stack>
-  )
-}
+      <Input type='number' placeholder="Please enter a character weapon" />
+    </Stack>
+  );
+};
